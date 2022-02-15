e3c2f4c8952e4c6574495e92b8cc12ec89ad45f4
yujin60697 <juliet.jiny@gmail.com>
2022-02-15 12:04:33 +0900

    adding the feature receives the request headers parameter used an event object as input to the function

--- CFF_call_test.py
+++ CFF_call_test.py
@@ -24,71 +24,88 @@ def testFunction(session, **kwargs):
     # set variable
     functionName = kwargs["functionName"]
     etag = kwargs["etag"]
-    uri = parse.unquote(kwargs["athenaResult"]["uri"])
-    referer = parse.unquote(kwargs["athenaResult"]["referrer"])
-    user_agent = parse.unquote(kwargs["athenaResult"]["user_agent"])
-    queryString = parse.unquote(kwargs["athenaResult"]["query_string"])
-    cookies = parse.unquote(kwargs["athenaResult"]["cookie"])
-    host_header = parse.unquote(kwargs["athenaResult"]["host_header"])
-    clientIP = kwargs["athenaResult"]["request_ip"]
-
+    
+    # set initial request header values
+    uri = referrer = request_ip = user_agent = query_string = cookies = host_header = "-"
+    # declare an array for request headers(input values) of a test result ex. input(172.70.49.4, /, -)
+    req_header_values = []
+    
     # choose function template json (FunctionTemplate.json for viewer-request, FunctionTemplate_res.json for viewer-response)
     templateFileName = "FunctionTemplate.json" if kwargs["evenType"] == "viewer-request" else "FunctionTemplate_res.json"
-
     with open(templateFileName,"r") as f:
         #read json template
         data = json.load(f)
 
         #change values in json template
-        #clientIP
-        data["viewer"] = {}
-        data["viewer"]["ip"] = clientIP
+        for header in kwargs["headers"]:
+            #clientIP
+            if header == "request_ip":
+                request_ip = kwargs["athenaResult"][header]
+                req_header_values.append(request_ip)
+                data["viewer"] = {}
+                data["viewer"]["ip"] = request_ip
+            #method
+            if header == "method":
+                method = kwargs["athenaResult"][header]
+                req_header_values.append(method)
+                data["request"][header] = method
+            #uri
+            if header == "uri":
+                uri = parse.unquote(kwargs["athenaResult"][header])
+                req_header_values.append(uri)
+                data["request"][header] = uri
+            #querystring
+            if header == "query_string":
+                query_string = parse.unquote(kwargs["athenaResult"][header])
+                #req_header_values.append(query_string) # doesn't need to add it into req_header_values array cause uri includes query_string.
+                if query_string != "-":
+                    data["request"]["querystring"] = {}
+                    arrquery = query_string.split("&")
+                    for query in arrquery:
+                        tempQuery = query.split("=")
+                        key = tempQuery[0].strip()
+                        value = tempQuery[1].strip()
 
-        #method
-        data["request"]["method"] = kwargs["athenaResult"]["method"]
-
-        #uri
-        data["request"]["uri"] = uri
-
-        #querystring
-        if queryString != "-":
-            data["request"]["querystring"] = {}
-            arrquery = queryString.split("&")
-            for query in arrquery:
-                tempQuery = query.split("=")
-                key = tempQuery[0].strip()
-                value = tempQuery[1].strip()
-
-                data["request"]["querystring"][key] = {}
-                data["request"]["querystring"][key]["value"] = value
-
-        #header
-        data["request"]["headers"] = {}
-        #referer
-        if referer != "-":
-            data["request"]["headers"]["referer"] = {}
-            data["request"]["headers"]["referer"]["value"] = referer
-        #user-agent
-        if user_agent != "-":
-            data["request"]["headers"]["user-agent"] = {}
-            data["request"]["headers"]["user-agent"]["value"] = user_agent
-        #host
-        if host_header != "-":
-            data["request"]["headers"]["host"] = {}
-            data["request"]["headers"]["host"]["value"] = host_header
-        
-        #cookie
-        if cookies != "-":
-            data["request"]["cookies"] = {}
-            cookie = cookies
-            arrCookie = cookie.split(";")
-            for query in arrCookie:
-                tempCookie = query.split("=")
-                key = tempCookie[0].strip()
-                value = tempCookie[1].strip()
-
-                data["request"]["cookies"][key] = {}
-                data["request"]["cookies"][key]["value"] = value
+                        data["request"]["querystring"][key] = {}
+                        data["request"]["querystring"][key]["value"] = value
+            #header
+            if header in ["referrer", "user_agent", "host_header", "cookie"]:
+                data["request"]["headers"] = {}
+                #referer
+                if header == "referrer":
+                    referrer = parse.unquote(kwargs["athenaResult"][header])
+                    req_header_values.append(referrer)
+                    if referrer != "-":
+                        data["request"]["headers"]["referer"] = {}
+                        data["request"]["headers"]["referer"]["value"] = referrer
+                #user-agent
+                if header == "user_agent":
+                    user_agent = parse.unquote(kwargs["athenaResult"][header])
+                    req_header_values.append(user_agent)
+                    if user_agent != "-":
+                        data["request"]["headers"][header] = {}
+                        data["request"]["headers"][header]["value"] = user_agent
+                #host
+                if header == "host_header":
+                    host_header = parse.unquote(kwargs["athenaResult"][header])
+                    req_header_values.append(host_header)
+                    if host_header != "-":
+                        data["request"]["headers"]["host"] = {}
+                        data["request"]["headers"]["host"]["value"] = host_header
+                #cookie
+                if header == "cookie":
+                    cookies = parse.unquote(kwargs["athenaResult"][header])
+                    req_header_values.append(cookies)
+                    if cookies != "-":
+                        data["request"]["cookies"] = {}
+                        cookie = cookies
+                        arrCookie = cookie.split(";")
+                        for query in arrCookie:
+                            tempCookie = query.split("=")
+                            key = tempCookie[0].strip()
+                            value = tempCookie[1].strip()
+                            data["request"]["cookies"][key] = {}
+                            data["request"]["cookies"][key]["value"] = value
 
     #print input event_struction
     #print(json.dumps(data, default=json_default, indent=4))
@@ -108,11 +125,14 @@ def testFunction(session, **kwargs):
     status = "OK" if response["TestResult"]["FunctionErrorMessage"] == "" else "Err"
 
     #input(1.2.3.4, google.com, /ask) → output (Err, 90%)
-    url = uri if queryString == "-" else uri + "?" + queryString
+    if query_string != "-":
+        uri = uri + "?" + query_string
+        req_header_values[kwargs["headers"].index("uri")] = uri
 
     # highlight if cpu utilize more than 80%
     cpu = '\033[31m' + cpu + '\033[0m' if int(cpu) > 80 else cpu
 
-    print("input({}, {}, {}, {}, {}, {})".format(clientIP, host_header, url, referer, user_agent, cookies) + " --> output({}, {}%)".format(status, cpu))
+    # print test results
+    print('input(' + ', '.join([str(req_header) for req_header in req_header_values]) + ")" + " --> output({}, {}%)".format(status, cpu))
 
     return cpu, status
